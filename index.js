ab6b98eb083ec5e346dc6070bf946c382fef3f19
koopero <koopero@gmail.com>
2015-02-20 01:00:04 -0800

    Does paths, kinda.

--- index.js
+++ index.js
@@ -1,17 +1,144 @@
+module.exports = NodeCachePath;
+
 const
   // bobject = require('bobject'),
-  path = require('path')
+  _ = require('underscore'),
+  _s = require('underscore.string'),
+  pathlib = require('path'),
+  SEP = pathlib.sep
 ;
+
+_.mixin(_s);
+
 function NodeCachePath ( opt ) {
-  
-  function cachePath( file ) {
-    const 
-      absFile = path.resolve( file )
+
+  const
+    self = {}
+  ;
+
+
+  //opt.cacheDir = ensureTrailingSlash( opt.cacheDir );
+
+  opt.cacheAbs = _.startsWith( opt.cacheDir, SEP );
+
+  opt.baseDir = ensureTrailingSlash( opt.baseDir );
+
+  var
+    cacheDirSegments = opt.cacheDir.split( SEP ).filter( function(v){return !!v} );
+  ;
+
+
+
+
+  self.pathToCache = pathToCache;
+  self.cacheToPath = cacheToPath;
+
+  return self;
+
+  function resolve() {
+    return pathlib.resolve.apply( this, arguments );
+  }
+
+
+  function pathToCache( file ) {
+    var
+      absFile = resolve( file ),
+      absDir = pathlib.dirname( absFile ),
+      relToBase,
+      relToBasSegs,
+      dirFromBase,
+      name,
+      cacheDir,
+      cacheFile;
     ;
 
 
-    if ( baseDir ) {
-      if ( )
+    if ( opt.baseDir ) {
+      if ( _.startsWith( absFile, opt.baseDir ) ) {
+        relToBase = absFile.substr( opt.baseDir.length )
+      } else {
+        // Won't bother with files outside our base
+        return;
+      }
+    } else {
+      relToBase = absFile;
     }
+
+    relToBaseSegs = relToBase.split(SEP);
+
+
+    name = pathlib.basename( relToBase );
+    if ( !name )
+      return;
+
+    if ( opt.cacheExt )
+      name += opt.cacheExt;
+
+    if ( opt.cacheAbs ) {
+      cacheDir = opt.cacheDir;
+
+      dirFromBase = pathlib.dirname( relToBase );
+      dirFromBase = removeLeadingSlash( dirFromBase );
+
+      cacheDir = resolve( opt.cacheDir, dirFromBase );
+      cacheFile = resolve( cacheDir, name );
+
+    } else {
+      if ( _.intersection( relToBaseSegs, cacheDirSegments ).length ) {
+        return;
+      }
+      cacheDir = resolve( absDir, opt.cacheDir );
+      cacheFile = resolve( cacheDir, name );
+    }
+
+    return cacheFile;
   }
+
+
+  function cacheToPath( cachePath ) {
+    var
+      cacheName,
+      cacheDir,
+      pathName,
+      pathDir,
+      pathPath
+    ;
+
+    cachePath = resolve( cachePath );
+    cacheName = pathlib.basename( cachePath );
+    cacheDir = pathlib.dirname( cachePath );
+
+
+    if ( opt.cacheAbs ) {
+      pathDir = startsWith( cacheDir, opt.cacheDir );
+      if ( !pathDir )
+        return;
+
+    } else {
+      if ( !_.endsWith( cacheDir, opt.cacheDir ) )
+        return;
+
+
+      pathDir = cacheDir.substr( 0, cacheDir.length - opt.cacheDir.length );
+    }
+
+    pathName = cacheName;
+    if ( opt.cacheExt ) {
+      pathName = endsWith( pathName, opt.cacheExt  );
+    }
+
+    if ( !pathName )
+     return;
+
+    pathPath = resolve( pathDir, pathName );
+
+    return pathPath;
+  }
+
 }
+
+function endsWith( a, b ) { return a.substr( a.length - b.length ) == b ? a.substr( 0, a.length - b.length ) : undefined; }
+function startsWith( a, b ) { return a.substr( 0, b.length ) == b ? a.substr( b.length ) : undefined; }
+
+function removeLeadingSlash( str ) { return _.ltrim(str,SEP) }
+function ensureTrailingSlash( str ) {  return str ? _.rtrim(str,SEP)+SEP : undefined; }
--- test/path-test.js
+++ test/path-test.js
@@ -4,69 +4,125 @@ const
   eq = ass.equal
 ;
 
-describe( '#path', function () {
+describe( '#pathToCache', function () {
   it('should transform paths using relative dir', function () {
-    const 
+    const
       cache = CachePath( {
-        dir: '.cache'
+        cacheDir: '.cache'
       }),
-      path = cache.path
+      pathToCache = cache.pathToCache
     ;
 
-    eq( path( '/foo/bar' ), '/foo/.cache/bar' );
-    eq( path( '/foo/bar/baz' ), '/foo/bar/.cache/baz' );
-    eq( path( '/baz' ), '/.cache/baz' );
-    eq( path( '/foo/bar/.cache' ), undefined );
-    eq( path( '/foo/bar/.cache/baz' ), undefined );
+    eq( pathToCache( '/foo/bar' ), '/foo/.cache/bar' );
+    eq( pathToCache( '/foo/bar/baz' ), '/foo/bar/.cache/baz' );
+    eq( pathToCache( '/baz' ), '/.cache/baz' );
+    eq( pathToCache( '/foo/bar/.cache' ), undefined );
+    eq( pathToCache( '/foo/bar/.cache/baz' ), undefined );
   });
 
   it('should transform paths using multi-level relative dir', function () {
-    const 
+    const
       cache = CachePath( {
-        dir: '.cache/mp3'
+        cacheDir: '.cache/mp3'
       }),
-      path = cache.path
+      pathToCache = cache.pathToCache
     ;
 
-    eq( path( '/foo/bar' ), '/foo/.cache/mp3/bar' );
-    eq( path( '/foo/bar/baz' ), '/foo/bar/.cache/mp3/baz' );
-    eq( path( '/baz' ), '/.cache/mp3/baz' );
+    eq( pathToCache( '/foo/bar' ), '/foo/.cache/mp3/bar' );
+    eq( pathToCache( '/foo/bar/baz' ), '/foo/bar/.cache/mp3/baz' );
+    eq( pathToCache( '/baz' ), '/.cache/mp3/baz' );
 
-    eq( path( '/foo/bar/.cache' ), undefined );
-    eq( path( '/foo/bar/.cache/mp3' ), undefined );
+    eq( pathToCache( '/foo/bar/.cache' ), undefined );
+    eq( pathToCache( '/foo/bar/.cache/mp3' ), undefined );
   });
 
   it('should transform paths using absolute dir', function () {
-    const 
+    const
       cache = CachePath( {
-        dir: '/cache'
+        cacheDir: '/cache'
       }),
-      path = cache.path
+      pathToCache = cache.pathToCache
     ;
 
-    eq( path( '/foo/bar' ), '/cache/foo/bar' );
-    eq( path( '/foo/bar/baz' ), '/cache/foo/bar/baz' );
-    eq( path( '/cache' ), undefined );
-    eq( path( '/' ), undefined );
-    eq( path( '/cache/' ), undefined );
-    eq( path( '/cache/foo' ), undefined );
+    eq( pathToCache( '/foo/bar' ), '/cache/foo/bar' );
+    eq( pathToCache( '/foo/bar/baz' ), '/cache/foo/bar/baz' );
+  });
+
+  xit('should avoid translating file that are within the cache zone ', function () {
+    const
+      cache = CachePath( {
+        cacheDir: '/cache'
+      }),
+      pathToCache = cache.pathToCache
+    ;
+    eq( pathToCache( '/cache' ), undefined );
+    eq( pathToCache( '/' ), undefined );
+    eq( pathToCache( '/cache/' ), undefined );
+    eq( pathToCache( '/cache/foo' ), undefined );
+  });
+
+
+  it('should transform paths using absolute dir and extension', function () {
+    const
+      cache = CachePath( {
+        cacheDir: '/cache',
+        cacheExt: '.jpg'
+      }),
+      pathToCache = cache.pathToCache
+    ;
+
+    eq( pathToCache( '/foo/bar.mp4' ), '/cache/foo/bar.mp4.jpg' );
+    eq( pathToCache( '/foo/bar/baz' ), '/cache/foo/bar/baz.jpg' );
+
+    // I know this next one looks a little weird, but it's important to
+    // stay predictable.
+    eq( pathToCache( '/foo/bar/baz.jpg' ), '/cache/foo/bar/baz.jpg.jpg' );
+  });
+
+});
+
+describe( '#cacheToPath', function () {
+  it('should transform paths using relative dir', function () {
+    const cacheToPath = CachePath( { cacheDir: '.cache' } ).cacheToPath;
+
+    eq( cacheToPath( '/foo/.cache/bar' ),  '/foo/bar' );
+    eq( cacheToPath( '/foo/bar/.cache/baz' ), '/foo/bar/baz' );
+    eq( cacheToPath( '/.cache/baz' ), '/baz' );
+  });
+
+  it('should transform paths using multi-level relative dir', function () {
+    const cacheToPath = CachePath( { cacheDir: '.cache/mp3' } ).cacheToPath;
+
+    eq( cacheToPath( '/foo/.cache/mp3/bar' ), '/foo/bar' );
+    eq( cacheToPath( '/foo/bar/.cache/mp3/baz' ), '/foo/bar/baz' );
+    eq( cacheToPath( '/.cache/mp3/baz' ), '/baz' );
+
+    eq( cacheToPath( '/foo/bar/.cache' ), undefined );
+    eq( cacheToPath( '/foo/bar/.cache/mp3' ), undefined );
+  });
+
+  it('should transform paths using absolute dir', function () {
+    const cacheToPath = CachePath( { cacheDir: '/cache' } ).cacheToPath;
+
+    eq( cacheToPath( '/cache/foo/bar' ), '/foo/bar' );
+    eq( cacheToPath( '/cache/foo/bar/baz' ), '/foo/bar/baz' );
   });
 
   it('should transform paths using absolute dir and extension', function () {
-    const 
+    const
       cache = CachePath( {
-        dir: '/cache',
-        ext: '.jpg'
+        cacheDir: '/cache',
+        cacheExt: '.jpg'
       }),
-      path = cache.path
+      cacheToPath = cache.cacheToPath
     ;
 
-    eq( path( '/foo/bar.mp4' ), '/cache/foo/bar.mp4.jpg' );
-    eq( path( '/foo/bar/baz' ), '/cache/foo/bar/baz.jpg' );
+    eq( cacheToPath( '/cache/foo/bar.mp4.jpg' ), '/foo/bar.mp4' );
+    eq( cacheToPath( '/cache/foo/bar/baz.jpg' ), '/foo/bar/baz' );
 
     // I know this next one looks a little weird, but it's important to
-    // stay predictable. 
-    eq( path( '/foo/bar/baz.jpg' ), '/cache/foo/bar/baz.jpg.jpg' );
+    // stay predictable.
+    eq( cacheToPath( '/cache/foo/bar/baz.jpg.jpg' ), '/foo/bar/baz.jpg' );
   });
 
 });
